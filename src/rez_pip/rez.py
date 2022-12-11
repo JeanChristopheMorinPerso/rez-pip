667b82e4c6266fdec8239e76e183360a0956b077
Jean-Christophe Morin <jean_christophe_morin@hotmail.com>
2022-12-11 15:34:31 -0500

    Add package maker

--- src/rez_pip/rez.py
+++ src/rez_pip/rez.py
@@ -27,8 +27,8 @@ def make_root(variant, path):
 
         shutil.copyfile(src, dest)
 
-        # if _is_exe(src):
-        #     shutil.copystat(src, dest)
+    if _is_exe(src):
+        shutil.copystat(src, dest)
 
 
 def createPackage(
@@ -57,5 +57,62 @@ def createPackage(
             else config.local_packages_path
         )
 
-    # with rez.package_maker.make_package(name, packagesPath, make_root=make_root) as pkg:
-    #     pass
+    with rez.package_maker.make_package(name, packagesPath) as pkg:
+        # basics (version etc)
+        pkg.version = version
+
+        if dist.metadata["summary"]:
+            pkg.description = dist.metadata["summary"]
+
+        # requirements and variants
+        if requires:
+            pkg.requires = requires
+
+        if variant_requires:
+            pkg.variants = [variant_requires]
+
+        # commands
+        commands: list[str] = []
+        commands.append("env.PYTHONPATH.append('{root}/python')")
+
+        console_scripts = [
+            ep.name for ep in dist.entry_points if ep.group == "console_scripts"
+        ]
+        if console_scripts:
+            pkg.tools = console_scripts
+            commands.append("env.PATH.append('{root}/scripts')")
+
+        pkg.commands = "\n".join(commands)
+
+        # Make the package use hashed variants. This is required because we
+        # can't control what ends up in its variants, and that can easily
+        # include problematic chars (>, +, ! etc).
+        # TODO: #672
+        #
+        pkg.hashed_variants = True
+
+        # add some custom attributes to retain pip-related info
+        pkg.pip_name = f"{dist.name}-{dist.version}"
+        pkg.from_pip = True
+        pkg.is_pure_python = metadata["is_pure_python"]
+
+        # distribution_metadata = dist.metadata.json
+
+        # help_ = []
+
+        # if "home_page" in distribution_metadata:
+        #     help_.append(["Documentation", distribution_metadata["home_page"]])
+
+        # if "download_url" in distribution_metadata:
+        #     help_.append(["Source Code", distribution_metadata["download_url"]])
+
+        # if help_:
+        #     pkg.help = help_
+
+        if dist.metadata["Author"]:
+            author = dist.metadata["Author"]
+
+            if dist.metadata["Author-email"]:
+                author += f" <{dist.metadata['Author-email']}>"
+
+            pkg.authors = [author]
