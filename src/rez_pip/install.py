7984bd4fce00e3cdf7e82db972032f2d67946e30
Jean-Christophe Morin <jean_christophe_morin@hotmail.com>
2022-12-22 16:50:45 -0500

    First attempt at using custom sheban. Not yet tested on Windows

--- src/rez_pip/install.py
+++ src/rez_pip/install.py
@@ -1,18 +1,31 @@
+"""
+Code that takes care of installing (extracting) wheels.
+"""
+import io
 import os
 import sys
 import typing
+import zipfile
 import logging
 import sysconfig
 import importlib.metadata
 
 import installer
 import installer.utils
+import installer.records
+import installer.scripts
 import installer.sources
 import installer.destinations
 
 import rez_pip.pip
 
 _LOG = logging.getLogger(__name__)
+
+if typing.TYPE_CHECKING:
+    from typing import Literal
+
+    LauncherKind = Literal["posix", "win-ia32", "win-amd64", "win-arm", "win-arm64"]
+    ScriptSection = Literal["console", "gui"]
 
 
 def isWheelPure(source: installer.sources.WheelSource) -> bool:
@@ -49,14 +62,13 @@ def installWheel(
     wheelPath: typing.Union[str, os.PathLike[str]],
     target: str,
 ) -> tuple[importlib.metadata.Distribution, bool]:
-    """
-    Technically, target should be optional. We will always want to install in "pip install --target"
-    mode. So right now it's a CLI option for debugging purposes.
-    """
+    # TODO: Technically, target should be optional. We will always want to install in "pip install --target"
+    #       mode. So right now it's a CLI option for debugging purposes.
 
-    destination = installer.destinations.SchemeDictionaryDestination(
+    destination = WheelDestination(
         getSchemeDict(package.name, target),
-        interpreter=sys.executable,  # Can be used for shebang I think.
+        # TODO: Use Python from rez package, or simply use "/usr/bin/env python"?
+        interpreter=sys.executable,
         script_kind=installer.utils.get_launcher_kind(),
     )
 
@@ -82,3 +94,88 @@ def installWheel(
     dist = importlib.metadata.distribution(package.name)
 
     return dist, isPure
+
+
+# TODO: Document where this code comes from.
+class WheelDestination(installer.destinations.SchemeDictionaryDestination):
+    # Exactly the same as SchemeDictionaryDestination, but uses our custom Script class.
+    def write_script(
+        self, name: str, module: str, attr: str, section: "ScriptSection"
+    ) -> installer.records.RecordEntry:
+        """Write a script to invoke an entrypoint.
+        :param name: name of the script
+        :param module: module path, to load the entry point from
+        :param attr: final attribute access, for the entry point
+        :param section: Denotes the "entry point section" where this was specified.
+            Valid values are ``"gui"`` and ``"console"``.
+        :type section: str
+        - Generates a launcher using :any:`Script.generate`.
+        - Writes to the "scripts" scheme.
+        - Uses :py:meth:`SchemeDictionaryDestination.write_to_fs` for the
+          filesystem interaction.
+        """
+        script = Script(name, module, attr, section)
+        script_name, data = script.generate(self.interpreter, self.script_kind)
+
+        with io.BytesIO(data) as stream:
+            entry = self.write_to_fs(
+                installer.utils.Scheme("scripts"),
+                script_name,
+                stream,
+                is_executable=True,
+            )
+
+            path = self._path_with_destdir(
+                installer.utils.Scheme("scripts"), script_name
+            )
+            mode = os.stat(path).st_mode
+            mode |= (mode & 0o444) >> 2
+            os.chmod(path, mode)
+
+            return entry
+
+
+_SCRIPT_TEMPLATE = """\
+# -*- coding: utf-8 -*-
+import re
+import sys
+from {module} import {import_name}
+if __name__ == "__main__":
+    sys.argv[0] = re.sub(r"(-script\\.pyw|\\.exe)?$", "", sys.argv[0])
+    sys.exit({func_path}())
+"""
+
+
+# TODO: Document where this code comes from.
+class Script(installer.scripts.Script):
+    def generate(
+        self, executable: str, kind: "LauncherKind"
+    ) -> typing.Tuple[str, bytes]:
+        """Generate a launcher for this script.
+        :param executable: Path to the executable to invoke.
+        :param kind: Which launcher template should be used.
+            Valid values are ``"posix"``, ``"win-ia32"``, ``"win-amd64"`` and
+            ``"win-arm"``.
+        :type kind: str
+        :raises InvalidScript: if no appropriate template is available.
+        :return: The name and contents of the launcher file.
+        """
+        launcher = self._get_launcher_data(kind)
+        # shebang = _build_shebang(executable, forlauncher=bool(launcher))
+        # TODO: Should we instead just pass that value to WheelDestination?
+        shebang = b"#!/usr/bin/env python"
+        code = _SCRIPT_TEMPLATE.format(
+            module=self.module,
+            import_name=self.attr.split(".")[0],
+            func_path=self.attr,
+        ).encode("utf-8")
+
+        if launcher is None:
+            return (self.name, shebang + b"\n" + code)
+
+        stream = io.BytesIO()
+        with zipfile.ZipFile(stream, "w") as zf:
+            zf.writestr("__main__.py", code)
+        name = f"{self.name}.exe"
+        data = launcher + shebang + b"\n" + stream.getvalue()
+        return (name, data)
