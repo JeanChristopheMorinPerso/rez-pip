7e80b47805492f27c5414f2bf0082f6cd5bb5c60
Jean-Christophe Morin <38703886+JeanChristopheMorinPerso@users.noreply.github.com>
2023-11-17 20:58:33 -0500

    Add changelog to docs and fix external links (#68)
    Signed-off-by: Jean-Christophe Morin <jean_christophe_morin@hotmail.com>

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,22 +1,26 @@
-# 0.3.1 (2023-11-17)
+# Changelog
 
-* Fix decoding error when reading pip dependency report. See [PR #63](https://github.com/JeanChristopheMorinPerso/rez-pip/pull/63).
-* Update vendored pip from 23.2.1 to 23.3.1
+<!-- start-here-sphinx-start-after -->
 
-# 0.3.0 (2023-10-12)
+## 0.3.1 (2023-11-17)
+
+* Fix decoding error when reading pip dependency report (#63).
+* Update vendored pip from 23.2.1 to 23.3.1.
+
+## 0.3.0 (2023-10-12)
 
 Most notable changes:
-* Don't append system PATH to context when finding the python executables (#47)
-* Update pip from 23.1.2 to 23.2 (#44)
-* Ensure Windows short paths are resolved to long paths (#50)
-* Read rez-pip version from distribution metadata (#53)
+* Don't append system PATH to context when finding the python executables (#47).
+* Update pip from 23.1.2 to 23.2 (#44).
+* Ensure Windows short paths are resolved to long paths (#50).
+* Read rez-pip version from distribution metadata (#53).
 
-# 0.2.0 (2023-07-04)
+## 0.2.0 (2023-07-04)
 
 This release fixes a lot of bugs and gets us closer to a more official release.
 
 Most notable changes:
-* Dropped support for installing Python 2 packages. Python 3.7+ is supported. See [PR #39](https://github.com/JeanChristopheMorinPerso/rez-pip/pull/39).
+* Dropped support for installing Python 2 packages. Python 3.7+ is supported (#39).
 * A lot more documentation. It now includes a transition guide, a user guide, information on the metadata conversion process, etc.
 * The command line arguments have been cleaned up (`--install-path` renamed to `--prefix`, `--release` was added, etc).
 * A lightweigth weehl cache was added to avoid re-downloading the same wheel for multiple python versions.
--- docs/source/conf.py
+++ docs/source/conf.py
@@ -3,6 +3,11 @@
 # For the full list of built-in configuration values, see the documentation:
 # https://www.sphinx-doc.org/en/master/usage/configuration.html
 
+from sphinx.application import Sphinx
+from sphinx.transforms import SphinxTransform
+from docutils.nodes import reference, Text
+import re
+
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
@@ -21,6 +26,7 @@ extensions = [
     "sphinx.ext.autosectionlabel",
     # Third-part
     "sphinx_inline_tabs",
+    "myst_parser",
 ]
 
 templates_path = ["_templates"]
@@ -33,7 +39,59 @@ exclude_patterns = []
 html_theme = "furo"
 html_static_path = ["_static"]
 
+
 # -- Options for sphinx.ext.autosectionlabel ---------------------------------
 # https://www.sphinx-doc.org/en/master/usage/extensions/autosectionlabel.html
 
 autosectionlabel_prefix_document = True
+
+
+# -- Options for the linkcheck builder ---------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-the-linkcheck-builder
+
+linkcheck_allowed_redirects = {
+    r"https://github.com/JeanChristopheMorinPerso/rez-pip/issues/\d+": "https://github.com/JeanChristopheMorinPerso/rez-pip/pull/\d+"
+}
+
+
+# -- Options for sphinx.ext.intersphinx_mapping ------------------------------
+# https://www.sphinx-doc.org/en/master/usage/extensions/intersphinx.html
+
+intersphinx_mapping = {
+    "rez": ("https://rez.readthedocs.io/en/stable/", None),
+}
+
+# Force usage of :external:
+intersphinx_disabled_reftypes = ["*"]
+
+
+# -- Custom ------------------------------------------------------------------
+# Custom stuff
+
+
+class ReplaceGHRefs(SphinxTransform):
+    default_priority = 750
+    prefix = "https://github.com/JeanChristopheMorinPerso/rez-pip/issues"
+
+    def apply(self):
+        istext = lambda o: isinstance(o, Text)
+
+        for node in self.document.traverse(istext):
+            match = re.match(r".*\((\#(\d+))\)\.?$", str(node))
+            if not match:
+                continue
+
+            newtext = Text(str(node)[: match.start(1)])
+            newreference = reference(
+                "", match.group(1), refuri=f"{self.prefix}/{match.group(2)}"
+            )
+            trailingtext = Text(str(node)[match.end(1) :])
+            node.parent.replace(node, newtext)
+            node.parent.insert(node.parent.index(newtext) + 1, newreference)
+            node.parent.insert(node.parent.index(newtext) + 2, trailingtext)
+
+        return
+
+
+def setup(app: Sphinx):
+    app.add_transform(ReplaceGHRefs)
