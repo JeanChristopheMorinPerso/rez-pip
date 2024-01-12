JeanChristopheMorinPerso/rez-pip

commit 9a7657470a62e52d149f728fa19335b217fdbe9a

refacto(main): extract new pip_install_packages function
reduce the size of the rez_install_pip_packages and allow for more re-usability as a python library

src/rez_pip/main.py
@@ -22,6 +22,71 @@ import rez_pip.exceptions
 _LOG = logging.getLogger(__name__)
 
 
+def pip_install_packages(
+    pipPackages: list[str],
+    pythonVersion: str,
+    pythonExecutable: pathlib.Path,
+    pipPath: str,
+    wheelsDir: str,
+    installedWheelsDir: str,
+    pipArgs: typing.Optional[typing.List[str]] = None,
+    requirementPath: typing.Optional[list[str]] = None,
+    constraintPath: typing.Optional[list[str]] = None,
+) -> typing.Dict[importlib_metadata.Distribution, bool]:
+    """
+    Install the given pip packages for the given python version using their wheels.
+
+    :param pipPackages: list of packages name to install, in the syntax understood by pip.
+    :param pythonVersion: exact python rez package version (absolute version).
+    :param pythonExecutable:
+        filesystem path to the python executable corresponding to the given version
+    :param pipPath: filesystem path to the pip executable. If not provided use the bundled pip.
+    :param wheelsDir: filesystem path to an existing directory to use for downloading wheels
+    :param installedWheelsDir: filesystem path to an existing directory to use for installing wheels
+    :param pipArgs: additional argument passed directly to pip
+    :param requirementPath: optional filesystem path to an existing python requirement file.
+    :param constraintPath: optional filesystem path to an existing python constraint file.
+    :return:
+        dict of an importlib Distribution instance for each pip package installed,
+        with the information if it's a pure python package:
+        ``dict[Distribution(), isPurePythonPackage]``
+    """
+    with rich.get_console().status(
+        f"[bold]Resolving dependencies for {rich.markup.escape(', '.join(pipPackages))} (python-{pythonVersion})"
+    ):
+        pipPackages = rez_pip.pip.getPackages(
+            pipPackages,
+            pipPath,
+            pythonVersion,
+            os.fspath(pythonExecutable),
+            requirementPath or [],
+            constraintPath or [],
+            pipArgs,
+        )
+
+    _LOG.info(f"Resolved {len(pipPackages)} dependencies for python {pythonVersion}")
+
+    # TODO: Should we postpone downloading to the last minute if we can?
+    _LOG.info("[bold]Downloading...")
+    wheels = rez_pip.download.downloadPackages(pipPackages, wheelsDir)
+    _LOG.info(f"[bold]Downloaded {len(wheels)} wheels")
+
+    dists: typing.Dict[importlib_metadata.Distribution, bool] = {}
+
+    with rich.get_console().status(
+        f"[bold]Installing wheels into {installedWheelsDir!r}"
+    ):
+        for package, wheel in zip(pipPackages, wheels):
+            _LOG.info(f"[bold]Installing {package.name}-{package.version} wheel")
+            dist, isPure = rez_pip.install.installWheel(
+                package, pathlib.Path(wheel), installedWheelsDir
+            )
+
+            dists[dist] = isPure
+
+    return dists
+
+
 def rez_install_pip_packages(
     pipPackages: list[str],
     pythonVersionRange: typing.Optional[str],
@@ -72,40 +137,17 @@ def rez_install_pip_packages(
         installedWheelsDir = os.path.join(pipWorkArea, "installed", pythonVersion)
         os.makedirs(installedWheelsDir, exist_ok=True)
 
-        with rich.get_console().status(
-            f"[bold]Resolving dependencies for {rich.markup.escape(', '.join(pipPackages))} (python-{pythonVersion})"
-        ):
-            pipPackages = rez_pip.pip.getPackages(
-                pipPackages,
-                pipPath,
-                pythonVersion,
-                os.fspath(pythonExecutable),
-                requirementPath or [],
-                constraintPath or [],
-                pipArgs,
-            )
-
-        _LOG.info(
-            f"Resolved {len(pipPackages)} dependencies for python {pythonVersion}"
+        dists = pip_install_packages(
+            pipPackages=pipPackages,
+            pythonVersion=pythonVersion,
+            pythonExecutable=pythonExecutable,
+            pipPath=pipPath,
+            wheelsDir=wheelsDir,
+            installedWheelsDir=installedWheelsDir,
+            pipArgs=pipArgs,
+            requirementPath=requirementPath,
+            constraintPath=constraintPath,
         )
-
-        # TODO: Should we postpone downloading to the last minute if we can?
-        _LOG.info("[bold]Downloading...")
-        wheels = rez_pip.download.downloadPackages(pipPackages, wheelsDir)
-        _LOG.info(f"[bold]Downloaded {len(wheels)} wheels")
-
-        dists: typing.Dict[importlib_metadata.Distribution, bool] = {}
-
-        with rich.get_console().status(
-            f"[bold]Installing wheels into {installedWheelsDir!r}"
-        ):
-            for package, wheel in zip(pipPackages, wheels):
-                _LOG.info(f"[bold]Installing {package.name}-{package.version} wheel")
-                dist, isPure = rez_pip.install.installWheel(
-                    package, pathlib.Path(wheel), installedWheelsDir
-                )
-
-                dists[dist] = isPure
 
         distNames = [dist.name for dist in dists.keys()]
 

commit 049c58046779ea2bf8329b8ffa163405d3a8958e

refacto(main): simplify pip_install_package
address issue introduced in 9a7657470a62e52d149f728fa19335b217fdbe9a where type were not as expected for the "list of pip packages"

src/rez_pip/main.py
@@ -24,49 +24,21 @@ _LOG = logging.getLogger(__name__)
 
 
 def pip_install_packages(
-    pipPackageNames: typing.List[str],
-    pythonVersion: str,
-    pythonExecutable: pathlib.Path,
-    pipPath: pathlib.Path,
+    pipPackages: typing.List[rez_pip.pip.PackageInfo],
     wheelsDir: pathlib.Path,
     installedWheelsDir: pathlib.Path,
-    pipArgs: typing.Optional[typing.List[str]] = None,
-    requirementPath: typing.Optional[typing.List[str]] = None,
-    constraintPath: typing.Optional[typing.List[str]] = None,
 ) -> typing.Dict[importlib_metadata.Distribution, bool]:
     """
     Install the given pip packages for the given python version using their wheels.
 
-    :param pipPackageNames: list of packages name to install, in the syntax understood by pip.
-    :param pythonVersion: exact python rez package version (absolute version).
-    :param pythonExecutable:
-        filesystem path to the python executable corresponding to the given version
-    :param pipPath: filesystem path to the pip executable. If not provided use the bundled pip.
+    :param pipPackages: list of packages install.
     :param wheelsDir: filesystem path to an existing directory to use for downloading wheels
     :param installedWheelsDir: filesystem path to an existing directory to use for installing wheels
-    :param pipArgs: additional argument passed directly to pip
-    :param requirementPath: optional filesystem path to an existing python requirement file.
-    :param constraintPath: optional filesystem path to an existing python constraint file.
     :return:
         dict of an importlib Distribution instance for each pip package installed,
         with the information if it's a pure python package:
         ``dict[Distribution(), isPurePythonPackage]``
     """
-    with rich.get_console().status(
-        f"[bold]Resolving dependencies for {rich.markup.escape(', '.join(pipPackageNames))} (python-{pythonVersion})"
-    ):
-        pipPackages = rez_pip.pip.getPackages(
-            pipPackageNames,
-            str(pipPath),
-            pythonVersion,
-            os.fspath(pythonExecutable),
-            requirementPath or [],
-            constraintPath or [],
-            pipArgs or [],
-        )
-
-    _LOG.info(f"Resolved {len(pipPackages)} dependencies for python {pythonVersion}")
-
     # TODO: Should we postpone downloading to the last minute if we can?
     _LOG.info("[bold]Downloading...")
     wheels = rez_pip.download.downloadPackages(pipPackages, str(wheelsDir))
@@ -102,7 +74,7 @@ def run_full_installation(
     """
     Convert the given pip packages to rez packages compatibe with the given python versions.
 
-    :param pipPackageNames: list of packages name to install, in the syntax understood by pip.
+    :param pipPackageNames: list of packages to install, in the syntax understood by pip.
     :param pythonVersionRange: a single or range of python versions in the rez syntax
     :param pipPath: filesystem path to the pip executable. If not provided use the bundled pip.
     :param requirementPath: optional filesystem path to an existing python requirement file.
@@ -127,7 +99,7 @@ def run_full_installation(
             f'No "python" package found within the range {pythonVersionRange!r}.'
         )
 
-    rezPackages = {}
+    rezPackages: typing.Dict[str, rez.package_maker.PackageMaker] = {}
 
     for pythonVersion, pythonExecutable in pythonVersions.items():
         _LOG.info(
@@ -142,22 +114,33 @@ def run_full_installation(
         installedWheelsDir = pipWorkArea / "installed" / pythonVersion
         os.makedirs(installedWheelsDir, exist_ok=True)
 
+        with rich.get_console().status(
+            f"[bold]Resolving dependencies for {rich.markup.escape(', '.join(pipPackageNames))} (python-{pythonVersion})"
+        ):
+            pipPackages = rez_pip.pip.getPackages(
+                pipPackageNames,
+                str(pipPath),
+                pythonVersion,
+                os.fspath(pythonExecutable),
+                requirementPath or [],
+                constraintPath or [],
+                pipArgs or [],
+            )
+
+        _LOG.info(
+            f"Resolved {len(pipPackages)} dependencies for python {pythonVersion}"
+        )
+
         dists = pip_install_packages(
-            pipPackageNames=pipPackageNames,
-            pythonVersion=pythonVersion,
-            pythonExecutable=pythonExecutable,
-            pipPath=pipPath,
+            pipPackages=pipPackages,
             wheelsDir=wheelsDir,
             installedWheelsDir=installedWheelsDir,
-            pipArgs=pipArgs,
-            requirementPath=requirementPath,
-            constraintPath=constraintPath,
         )
 
         distNames = [dist.name for dist in dists.keys()]
 
         with rich.get_console().status("[bold]Creating rez packages..."):
-            for dist, package in zip(dists, pipPackageNames):
+            for dist, package in zip(dists, pipPackages):
                 isPure = dists[dist]
                 rezPackage = rez_pip.rez.createPackage(
                     dist,