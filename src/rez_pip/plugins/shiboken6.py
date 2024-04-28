f41fb6da58217d85d3aadd0ef7de2219daf8bc8d
Jean-Christophe Morin <jean_christophe_morin@hotmail.com>
2024-04-28 16:45:38 -0400

    Fix remaining issues (I think)
    Signed-off-by: Jean-Christophe Morin <jean_christophe_morin@hotmail.com>

--- src/rez_pip/plugins/shiboken6.py
+++ src/rez_pip/plugins/shiboken6.py
@@ -1,6 +1,7 @@
 import os
 import sys
 import shutil
+import logging
 
 import packaging.utils
 
@@ -11,10 +12,19 @@ if sys.version_info >= (3, 10):
 else:
     import importlib_metadata
 
+_LOG = logging.getLogger(__name__)
+
 
 @rez_pip.plugins.hookimpl
 def cleanup(dist: importlib_metadata.Distribution, path: str) -> None:
-    if packaging.utils.canonicalize_name(dist.name) == "shiboken6":
-        path = os.path.join(path, "python", "PySide6")
-        print(f"Removing {path!r}")
+    if packaging.utils.canonicalize_name(dist.name) != "shiboken6":
+        return
+
+    # Remove PySide6 from shiboken6 packages...
+    # shiboken6 >=6.3, <6.6.2 were shipping some PySide6 folders by mistake.
+    # Not removing these extra folders would stop python from being able to import
+    # the correct PySide6 (that lives in a separate rez package).
+    path = os.path.join(path, "python", "PySide6")
+    if os.path.exists(path):
+        _LOG.debug(f"Removing {path!r}")
         shutil.rmtree(path)
