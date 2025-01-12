5b76ca717a715a42c96893f7d7a35ccfd7db6f57
Jean-Christophe Morin <jean_christophe_morin@hotmail.com>
2025-01-12 14:32:01 -0500

    Adjust shiboken cleanup hook

--- src/rez_pip/plugins/shiboken6.py
+++ src/rez_pip/plugins/shiboken6.py
@@ -19,15 +19,16 @@ _LOG = logging.getLogger(__name__)
 
 
 @rez_pip.plugins.hookimpl
-def cleanup(dist: "importlib_metadata.Distribution", path: str) -> None:
+def cleanup(
+    dist: "importlib_metadata.Distribution", path: str
+) -> typing.List[rez_pip.plugins.CleanupAction]:
     if packaging.utils.canonicalize_name(dist.name) != "shiboken6":
-        return
+        return []
 
     # Remove PySide6 from shiboken6 packages...
     # shiboken6 >=6.3, <6.6.2 were shipping some PySide6 folders by mistake.
     # Not removing these extra folders would stop python from being able to import
     # the correct PySide6 (that lives in a separate rez package).
-    path = os.path.join(path, "python", "PySide6")
-    if os.path.exists(path):
-        _LOG.debug(f"Removing {path!r}")
-        shutil.rmtree(path)
+    return [
+        rez_pip.plugins.CleanupAction("remove", os.path.join(path, "python", "PySide6"))
+    ]
