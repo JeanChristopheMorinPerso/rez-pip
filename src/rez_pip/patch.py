887b1bda607b4c22e376fdf7135515543fd9d8a7
Jean-Christophe Morin <jean_christophe_morin@hotmail.com>
2025-01-12 16:07:29 -0500

    Log patch_ng logs if applying a patch fails
    Signed-off-by: Jean-Christophe Morin <jean_christophe_morin@hotmail.com>

--- src/rez_pip/patch.py
+++ src/rez_pip/patch.py
@@ -1,16 +1,21 @@
 from __future__ import annotations
 
 import os
+import math
 import typing
 import logging
+import contextlib
+import logging.handlers
 
 import patch_ng
 
-import rez_pip.data.patches
+import rez_pip.utils
 import rez_pip.compat
 import rez_pip.plugins
 import rez_pip.exceptions
+import rez_pip.data.patches
 from rez_pip.compat import importlib_metadata
+
 
 _LOG = logging.getLogger(__name__)
 
@@ -22,6 +27,40 @@ class PatchError(rez_pip.exceptions.RezPipError):
 def getBuiltinPatchesDir() -> str:
     """Get the built-in patches directory"""
     return os.path.dirname(rez_pip.data.patches.__file__)
+
+
+@contextlib.contextmanager
+def logIfErrorOrRaises() -> typing.Generator[None]:
+    """
+    Log patch_ng logs if any error is logged or if the wrapped body raises.
+    Very slightly inspired by https://docs.python.org/3/howto/logging-cookbook.html#buffering-logging-messages-and-outputting-them-conditionally
+
+    We basically don't want any logs from patch_ng is everything worked. We only
+    want logs when something wrong happens.
+    """
+    patch_ng.debugmode = True
+    logger = logging.getLogger("patch_ng")
+    initialLevel = logger.level
+    logger.setLevel(logging.DEBUG)
+
+    handler = logging.handlers.MemoryHandler(
+        math.inf,  # type: ignore[arg-type]
+        flushLevel=logging.ERROR,
+        target=logging.getLogger("rez_pip").handlers[0],
+    )
+    handler.setFormatter(logging.Formatter("%(name)s %(levelname)8s %(message)s"))
+
+    logger.addHandler(handler)
+
+    try:
+        yield
+    except Exception as exc:
+        handler.flush()
+        raise exc from None
+    finally:
+        patch_ng.debugmode = False
+        logger.setLevel(initialLevel)
+        logger.removeHandler(handler)
 
 
 def patch(dist: importlib_metadata.Distribution, path: str) -> None:
@@ -50,5 +89,7 @@ def patch(dist: importlib_metadata.Distribution, path: str) -> None:
             raise PatchError(f"Patch at {patch!r} does not exist")
 
         patchset = patch_ng.fromfile(patch)
-        if not patchset.apply(root=path):
-            raise PatchError(f"Failed to apply patch {patch!r} on {path!r}")
+        with logIfErrorOrRaises():
+            if not patchset.apply(root=path):
+                # A logger that only gets flushed on demand would be better...
+                raise PatchError(f"Failed to apply patch {patch!r} on {path!r}")
