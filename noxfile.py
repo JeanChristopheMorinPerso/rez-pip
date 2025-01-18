1342de0b619a20eb7a986164a326977439e23381
Jean-Christophe Morin <jean_christophe_morin@hotmail.com>
2025-01-18 15:59:13 -0500

    Start to remove support for Python 3.7

--- noxfile.py
+++ noxfile.py
@@ -26,7 +26,7 @@ def format(session: nox.Session):
     session.run("black", ".", "--check")
 
 
-@nox.session(python=["3.7", "3.8", "3.9", "3.10", "3.11"])
+@nox.session(python=["3.8", "3.9", "3.10", "3.11"])
 def test(session: nox.Session):
     session.install("-r", "tests/requirements.txt")
     session.install(".")
