JeanChristopheMorinPerso/rez-pip

commit ce91594426e732b1d07466c635ee5ae410e4a35b

Add plugin system and local wheels support (#91)
Signed-off-by: Jean-Christophe Morin <jean_christophe_morin@hotmail.com>

src/rez_pip/cli.py
@@ -1,3 +1,5 @@
+from __future__ import annotations
+
 import os
 import sys
 import json
@@ -6,18 +8,13 @@ import typing
 import logging
 import argparse
 import textwrap
-import pathlib
 import tempfile
+import itertools
 import subprocess
 
-if sys.version_info >= (3, 10):
-    import importlib.metadata as importlib_metadata
-else:
-    import importlib_metadata
-
-import rich
 import rich.text
 import rich.panel
+import rich.table
 import rez.version
 import rich.markup
 import rich.logging
@@ -25,9 +22,13 @@ import rich.logging
 import rez_pip.pip
 import rez_pip.rez
 import rez_pip.data
+import rez_pip.patch
+import rez_pip.utils
+import rez_pip.plugins
 import rez_pip.install
 import rez_pip.download
 import rez_pip.exceptions
+from rez_pip.compat import importlib_metadata
 
 _LOG = logging.getLogger("rez_pip.cli")
 
@@ -120,6 +121,10 @@ def _createParser() -> argparse.ArgumentParser:
         help="Print debug information that you can use when reporting an issue on GitHub.",
     )
 
+    debugGroup.add_argument(
+        "--list-plugins", action="store_true", help="List all registered plugins"
+    )
+
     parser.usage = f"""
 
   %(prog)s [options] <package(s)>
@@ -188,7 +193,7 @@ def _run(args: argparse.Namespace, pipArgs: typing.List[str], pipWorkArea: str) 
         installedWheelsDir = os.path.join(pipWorkArea, "installed", pythonVersion)
         os.makedirs(installedWheelsDir, exist_ok=True)
 
-        with rich.get_console().status(
+        with rez_pip.utils.CONSOLE.status(
             f"[bold]Resolving dependencies for {rich.markup.escape(', '.join(args.packages))} (python-{pythonVersion})"
         ):
             packages = rez_pip.pip.getPackages(
@@ -202,44 +207,77 @@ def _run(args: argparse.Namespace, pipArgs: typing.List[str], pipWorkArea: str) 
             )
 
         _LOG.info(f"Resolved {len(packages)} dependencies for python {pythonVersion}")
+        _packageGroups: typing.List[
+            rez_pip.pip.PackageGroup[rez_pip.pip.PackageInfo]
+        ] = list(
+            itertools.chain(*rez_pip.plugins.getHook().groupPackages(packages=packages))  # type: ignore[arg-type]
+        )
+
+        # TODO: Verify that no packages are in two or more groups? It should theorically
+        # not be possible since plugins are called one after the other? But it could happen
+        # if a plugin forgets to pop items from the package list... The problem is that we
+        # can't know which plugin did what, so we could only say "something went wrong"
+        # and can't point to which plugin is at fault.
+
+        # Remove empty groups
+        _packageGroups = [group for group in _packageGroups if group]
+
+        # Add packages that were not grouped.
+        _packageGroups += [
+            rez_pip.pip.PackageGroup[rez_pip.pip.PackageInfo](tuple([package]))
+            for package in packages
+        ]
 
         # TODO: Should we postpone downloading to the last minute if we can?
         _LOG.info("[bold]Downloading...")
-        wheels = rez_pip.download.downloadPackages(packages, wheelsDir)
-        _LOG.info(f"[bold]Downloaded {len(wheels)} wheels")
 
-        dists: typing.Dict[importlib_metadata.Distribution, bool] = {}
+        packageGroups: typing.List[
+            rez_pip.pip.PackageGroup[rez_pip.pip.DownloadedArtifact]
+        ] = rez_pip.download.downloadPackages(_packageGroups, wheelsDir)
 
-        with rich.get_console().status(
+        foundLocally = downloaded = 0
+        for group in packageGroups:
+            for package in group.packages:
+                if not package.isDownloadRequired():
+                    foundLocally += 1
+                else:
+                    downloaded += 1
+
+        _LOG.info(
+            f"[bold]Downloaded {downloaded} wheels, skipped {foundLocally} because they resolved to local files"
+        )
+
+        with rez_pip.utils.CONSOLE.status(
             f"[bold]Installing wheels into {installedWheelsDir!r}"
         ):
-            for package, wheel in zip(packages, wheels):
-                _LOG.info(f"[bold]Installing {package.name}-{package.version} wheel")
-                dist, isPure = rez_pip.install.installWheel(
-                    package, pathlib.Path(wheel), installedWheelsDir
-                )
+            for group in packageGroups:
+                for package in group.packages:
+                    _LOG.info(f"[bold]Installing {package.name!r} {package.path!r}")
+                    targetPath = os.path.join(installedWheelsDir, package.name)
+                    dist = rez_pip.install.installWheel(
+                        package,
+                        package.path,
+                        targetPath,
+                    )
 
-                dists[dist] = isPure
+                    rez_pip.install.cleanup(dist, targetPath)
+                    rez_pip.patch.patch(dist, targetPath)
 
-        distNames = [dist.name for dist in dists.keys()]
+                    group.dists.append(dist)
 
-        with rich.get_console().status("[bold]Creating rez packages..."):
-            for dist, package in zip(dists, packages):
-                isPure = dists[dist]
+        with rez_pip.utils.CONSOLE.status("[bold]Creating rez packages..."):
+            for group in packageGroups:
                 rez_pip.rez.createPackage(
-                    dist,
-                    isPure,
+                    group,
                     rez.version.Version(pythonVersion),
-                    distNames,
                     installedWheelsDir,
-                    wheelURL=package.download_info.url,
                     prefix=args.prefix,
                     release=args.release,
                 )
 
 
 def _debug(
-    args: argparse.Namespace, console: rich.console.Console = rich.get_console()
+    args: argparse.Namespace, console: rich.console.Console = rez_pip.utils.CONSOLE
 ) -> None:
     """Print debug information"""
     prefix = "  "
@@ -313,9 +351,23 @@ def _debug(
     )
 
 
+def _printPlugins() -> None:
+    table = rich.table.Table("Name", "Hooks", box=None)
+    for plugin, hooks in rez_pip.plugins._getHookImplementations().items():
+        table.add_row(plugin, ", ".join(hooks))
+    rez_pip.utils.CONSOLE.print(table)
+
+
 def run() -> int:
     pipWorkArea = tempfile.mkdtemp(prefix="rez-pip-target")
     args, pipArgs = _parseArgs(sys.argv[1:])
+
+    # Initialize the plugin system
+    rez_pip.plugins.getManager()
+
+    if args.list_plugins:
+        _printPlugins()
+        return 0
 
     try:
         _validateArgs(args)
@@ -336,7 +388,7 @@ def run() -> int:
         _run(args, pipArgs, pipWorkArea)
         return 0
     except rez_pip.exceptions.RezPipError as exc:
-        rich.get_console().print(exc, soft_wrap=True)
+        rez_pip.utils.CONSOLE.print(exc, soft_wrap=True)
         return 1
     finally:
         if not args.keep_tmp_dirs:

src/rez_pip/download.py
@@ -1,38 +1,37 @@
+from __future__ import annotations
+
 import os
-import sys
 import typing
 import asyncio
 import hashlib
 import logging
 
-import rich
 import aiohttp
 import rich.progress
 
-if sys.version_info >= (3, 10):
-    import importlib.metadata as importlib_metadata
-else:
-    import importlib_metadata
-
 import rez_pip.pip
+import rez_pip.utils
+from rez_pip.compat import importlib_metadata
 
 _LOG = logging.getLogger(__name__)
 _lock = asyncio.Lock()
 
 
 def downloadPackages(
-    packages: typing.List[rez_pip.pip.PackageInfo], dest: str
-) -> typing.List[str]:
-    return asyncio.run(_downloadPackages(packages, dest))
+    packageGroups: typing.List[rez_pip.pip.PackageGroup[rez_pip.pip.PackageInfo]],
+    dest: str,
+) -> typing.List[rez_pip.pip.PackageGroup[rez_pip.pip.DownloadedArtifact]]:
+    return asyncio.run(_downloadPackages(packageGroups, dest))
 
 
 async def _downloadPackages(
-    packages: typing.List[rez_pip.pip.PackageInfo], dest: str
-) -> typing.List[str]:
-    items: typing.List[
-        typing.Coroutine[typing.Any, typing.Any, typing.Optional[str]]
+    packageGroups: typing.List[rez_pip.pip.PackageGroup[rez_pip.pip.PackageInfo]],
+    dest: str,
+) -> list[rez_pip.pip.PackageGroup[rez_pip.pip.DownloadedArtifact]]:
+    newPackageGroups: typing.List[
+        rez_pip.pip.PackageGroup[rez_pip.pip.DownloadedArtifact]
     ] = []
-    wheels = []
+    someFailed = False
 
     async with aiohttp.ClientSession() as session:
         with rich.progress.Progress(
@@ -42,30 +41,90 @@ async def _downloadPackages(
             rich.progress.DownloadColumn(),
             rich.progress.TransferSpeedColumn(),
             transient=True,
-            console=rich.get_console(),
+            console=rez_pip.utils.CONSOLE,
         ) as progress:
             tasks: typing.Dict[str, rich.progress.TaskID] = {}
 
-            # Create all the downlod tasks first
-            for package in packages:
-                tasks[package.name] = progress.add_task(package.name)
+            # Create all the download tasks first
+            numPackages = 0
+            for group in packageGroups:
+                for package in group.packages:
+                    if not package.isDownloadRequired():
+                        continue
+
+                    numPackages += 1
+                    tasks[package.name] = progress.add_task(package.name)
 
             # Then create the "total" progress bar. This ensures that total is at the bottom.
-            mainTask = progress.add_task(f"[bold]Total (0/{len(packages)})", total=0)
+            mainTask = progress.add_task(f"[bold]Total (0/{numPackages})", total=0)
 
-            for package in packages:
-                items.append(
-                    _download(
-                        package, dest, session, progress, tasks[package.name], mainTask
-                    )
+            futureGroups: list[
+                list[
+                    typing.Coroutine[
+                        typing.Any,
+                        typing.Any,
+                        rez_pip.pip.DownloadedArtifact | None,
+                    ]
+                ]
+            ] = []
+
+            # loop = asyncio.get_event_loop()
+            for group in packageGroups:
+                futures: list[
+                    typing.Coroutine[
+                        typing.Any,
+                        typing.Any,
+                        rez_pip.pip.DownloadedArtifact | None,
+                    ]
+                ] = []
+                for package in group.packages:
+                    wheelName: str = os.path.basename(package.download_info.url)
+                    wheelPath = os.path.join(dest, wheelName)
+
+                    if not package.isDownloadRequired():
+
+                        # Note the subtlety of having to pass variables in the function
+                        # signature. We can't rely on the scoped variable.
+                        async def _return_local(
+                            _wheelPath: str, _package: rez_pip.pip.PackageInfo
+                        ) -> rez_pip.pip.DownloadedArtifact:
+                            return rez_pip.pip.DownloadedArtifact.from_dict(
+                                {"_localPath": _wheelPath, **_package.to_dict()}
+                            )
+
+                        futures.append(_return_local(wheelPath, package))
+                    else:
+                        futures.append(
+                            _download(
+                                package,
+                                session,
+                                progress,
+                                tasks[package.name],
+                                mainTask,
+                                wheelName,
+                                wheelPath,
+                            )
+                        )
+                futureGroups.append(futures)
+
+            for _futures in futureGroups:
+                artifacts = tuple(await asyncio.gather(*_futures))
+
+                if not all(artifacts):
+                    someFailed = True
+
+                artifacts = typing.cast(
+                    typing.Tuple[rez_pip.pip.DownloadedArtifact], artifacts
                 )
 
-            wheels = await asyncio.gather(*items)
+                newPackageGroups.append(
+                    rez_pip.pip.PackageGroup[rez_pip.pip.DownloadedArtifact](artifacts)
+                )
 
-    if not all(wheels):
+    if someFailed:
         raise RuntimeError("Some wheels failed to be downloaded")
 
-    return typing.cast(typing.List[str], wheels)
+    return newPackageGroups
 
 
 def getSHA256(path: str) -> str:
@@ -86,15 +145,13 @@ def getSHA256(path: str) -> str:
 
 async def _download(
     package: rez_pip.pip.PackageInfo,
-    target: str,
     session: aiohttp.ClientSession,
     progress: rich.progress.Progress,
     taskID: rich.progress.TaskID,
     mainTaskID: rich.progress.TaskID,
-) -> typing.Optional[str]:
-    wheelName: str = os.path.basename(package.download_info.url)
-    wheelPath = os.path.join(target, wheelName)
-
+    wheelName: str,
+    wheelPath: str,
+) -> rez_pip.pip.DownloadedArtifact | None:
     # TODO: Handle case where sha256 doesn't exist. We should also support the other supported
     # hash types.
     if (
@@ -152,4 +209,6 @@ async def _download(
             mainTaskID, description=f"[bold]Total ({len(completedItems)}/{total})"
         )
 
-    return wheelPath
+    return rez_pip.pip.DownloadedArtifact.from_dict(
+        {"_localPath": wheelPath, **package.to_dict()}
+    )

.coveragerc
@@ -1,0 +1,9 @@
+[run]
+branch = True
+source_pkgs=rez_pip
+
+[report]
+exclude_also =
+    def __dir__
+    if TYPE_CHECKING:
+    if typing.TYPE_CHECKING:

.gitattributes
@@ -1,0 +1,5 @@
+# Set the default behavior, in case people don't have core.autocrlf set.
+* text=auto
+
+# Denote all files that are truly binary and should not be modified.
+*.patch binary

.gitignore
@@ -57,3 +57,4 @@ tests/data/rez_repo/
 tests/data/_tmp_download/
 docs/bin/
 .idea/
+/patches

docs/source/conf.py
@@ -4,14 +4,20 @@
 # https://www.sphinx-doc.org/en/master/usage/configuration.html
 
 import re
+import inspect
 import argparse
+import importlib
 
 import docutils.nodes
 import sphinx.transforms
+import sphinx.util.nodes
 import sphinx.application
+import sphinx.ext.autodoc
 import sphinx.util.docutils
+import docutils.statemachine
 
 import rez_pip.cli
+import rez_pip.plugins
 
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
@@ -25,6 +31,7 @@ author = "Rez Developers"
 
 extensions = [
     # first-party extensions
+    "sphinx.ext.todo",
     "sphinx.ext.autodoc",
     "sphinx.ext.extlinks",
     "sphinx.ext.intersphinx",
@@ -63,12 +70,25 @@ linkcheck_allowed_redirects = {
 # https://www.sphinx-doc.org/en/master/usage/extensions/intersphinx.html
 
 intersphinx_mapping = {
+    "python": ("https://docs.python.org/3", None),
     "rez": ("https://rez.readthedocs.io/en/stable/", None),
 }
 
 # Force usage of :external:
-intersphinx_disabled_reftypes = ["*"]
+# intersphinx_disabled_reftypes = ["*"]
 
+
+# -- Options for sphinx.ext.autodoc ------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/extensions/autodoc.html
+
+# autodoc_typehints = "description"
+autodoc_typehints_format = "short"
+autodoc_member_order = "bysource"
+
+# -- Options for sphinx.ext.todo --------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/extensions/todo.html
+
+todo_include_todos = True
 
 # -- Custom ------------------------------------------------------------------
 # Custom stuff
@@ -235,6 +255,167 @@ class RezAutoArgparseDirective(sphinx.util.docutils.SphinxDirective):
         return node.children
 
 
+class RezAutoPlugins(sphinx.util.docutils.SphinxDirective):
+    """
+    Special rez-pip-autoplugins directive. This is quite similar to "autosummary" in some ways.
+    """
+
+    required_arguments = 0
+    optional_arguments = 0
+
+    def run(self) -> list[docutils.nodes.Node]:
+        # Create the node.
+        node = docutils.nodes.section()
+        node.document = self.state.document
+
+        rst = docutils.statemachine.ViewList()
+
+        # Add rezconfig as a dependency to the current document. The document
+        # will be rebuilt if rezconfig changes.
+        self.env.note_dependency(rez_pip.plugins.__file__)
+        self.env.note_dependency(__file__)
+
+        path, lineNumber = self.get_source_info()
+
+        document = []
+        for plugin, hooks in rez_pip.plugins._getHookImplementations().items():
+            hooks = [f":func:`{hook}`" for hook in hooks]
+            document.append(f"* {plugin.split('.')[-1]}: {', '.join(hooks)}")
+
+        document = "\n".join(document)
+
+        # Add each line to the view list.
+        for index, line in enumerate(document.split("\n")):
+            # Note to future people that will look at this.
+            # "line" has to be a single line! It can't be a line like "this\nthat".
+            rst.append(line, path, lineNumber + index)
+
+        # Finally, convert the rst into the appropriate docutils/sphinx nodes.
+        sphinx.util.nodes.nested_parse_with_titles(self.state, rst, node)
+
+        # Return the generated nodes.
+        return node.children
+
+
+class RezPipAutoPluginHooks(sphinx.util.docutils.SphinxDirective):
+    """
+    Special rez-pip-autopluginhooks directive. This is quite similar to "autosummary" in some ways.
+    """
+
+    required_arguments = 1
+    optional_arguments = 0
+
+    def run(self) -> list[docutils.nodes.Node]:
+        # Create the node.
+        node = docutils.nodes.section()
+        node.document = self.state.document
+
+        rst = docutils.statemachine.ViewList()
+
+        # Add rezconfig as a dependency to the current document. The document
+        # will be rebuilt if rezconfig changes.
+        self.env.note_dependency(rez_pip.plugins.__file__)
+        self.env.note_dependency(__file__)
+
+        path, lineNumber = self.get_source_info()
+
+        fullyQualifiedClassName = self.arguments[0]
+        module, klassname = fullyQualifiedClassName.rsplit(".", 1)
+
+        mod = importlib.import_module(module)
+        klass = getattr(mod, klassname)
+
+        methods = [
+            method
+            for method in inspect.getmembers(klass, predicate=inspect.isfunction)
+            if not method[0].startswith("_")
+        ]
+
+        document = []
+        for method in sorted(methods, key=lambda x: x[1].__code__.co_firstlineno):
+            document.append(f".. autohook:: {module}.{klassname}.{method[0]}")
+
+        document = "\n".join(document)
+
+        # Add each line to the view list.
+        for index, line in enumerate(document.split("\n")):
+            # Note to future people that will look at this.
+            # "line" has to be a single line! It can't be a line like "this\nthat".
+            rst.append(line, path, lineNumber + index)
+
+        # Finally, convert the rst into the appropriate docutils/sphinx nodes.
+        sphinx.util.nodes.nested_parse_with_titles(self.state, rst, node)
+
+        # Return the generated nodes.
+        return node.children
+
+
+def autodoc_process_signature(
+    app: sphinx.application.Sphinx,
+    what: str,
+    name: str,
+    obj,
+    options: dict,
+    signature: str,
+    return_annotation,
+):
+    for name in ["Sequence", "Mapping", "MutableSequence"]:
+        signature = signature.replace(
+            f"rez_pip.compat.{name}", f"~collections.abc.{name}"
+        )
+        if return_annotation:
+            return_annotation = return_annotation.replace(
+                f"rez_pip.compat.{name}", f"~collections.abc.{name}"
+            )
+
+    signature = signature.replace(
+        "rez_pip.compat.importlib_metadata", "~importlib.metadata"
+    )
+
+    return signature, return_annotation
+
+
+class HookDocumenter(sphinx.ext.autodoc.FunctionDocumenter):
+    """
+    Custom autohook directive to document our hooks.
+    It allows us to easily document the hooks from the rez_pip.plugins.PluginSpec
+    class without exposing the class and module name.
+    """
+
+    objtype = "hook"  # auto + hook
+    directivetype = "function"  # generated reST directive
+
+    def format_signature(self, **kwargs) -> str:
+        """
+        Format the signature and remove self. We really don't want to expose
+        the class and module name or the fact that we are documenting methods.
+        """
+        sig = super().format_signature(**kwargs)
+        sig = re.sub(r"\(self(,\s)?", "(", sig)
+
+        # Also force short names for our own types
+        sig = sig.replace("rez_pip.", "~rez_pip.")
+
+        return sig
+
+    def add_directive_header(self, sig):
+        modname = self.modname
+        # Hacky, but it does the job. This should remove the module name from the directive
+        # created by autodoc.
+        self.modname = ""
+
+        data = super().add_directive_header(sig)
+
+        # We need to restore it because autodoc does lots of things with the module name.
+        self.modname = modname
+        return data
+
+
 def setup(app: sphinx.application.Sphinx):
     app.add_directive("rez-autoargparse", RezAutoArgparseDirective)
+    app.add_directive("rez-pip-autoplugins", RezAutoPlugins)
+    app.add_directive("rez-pip-autopluginhooks", RezPipAutoPluginHooks)
     app.add_transform(ReplaceGHRefs)
+
+    app.connect("autodoc-process-signature", autodoc_process_signature)
+    app.add_autodocumenter(HookDocumenter)

pyproject.toml
@@ -21,6 +21,12 @@ dependencies = [
     "dataclasses-json",
     "rich",
     "importlib_metadata>=4.6 ; python_version < '3.10'",
+    # 1.3 introduces type hints.
+    "pluggy>=1.2",
+    "typing-extensions; python_version < '3.8'",
+    # Patches are finicky... Let's lock on the current latest version.
+    # We could always relax later if needed.
+    "patch-ng==1.18.1",
 ]
 
 classifiers = [

pytest.ini
@@ -7,12 +7,12 @@ addopts =
     --cov-report=term-missing
     --cov-report=xml
     --cov-report=html
-    --durations=0
+    #--durations=0
 
 norecursedirs = rez_repo
 
 markers =
     integration: mark the tests as integration tests
     py37: mark the tests has using a Python 3.7 rez package
-    py39: mark the tests has using a Python 3.7 rez package
+    py39: mark the tests has using a Python 3.9 rez package
     py311: mark the tests has using a Python 3.11 rez package

commit 6a26a101c6871869133db9af60656a20e1202b3d

Fix logging bugs introduced by PR #91
Signed-off-by: Jean-Christophe Morin <jean_christophe_morin@hotmail.com>

src/rez_pip/cli.py
@@ -370,7 +370,10 @@ def run() -> int:
         _validateArgs(args)
 
         handler = rich.logging.RichHandler(
-            show_time=False, markup=True, show_path=False
+            show_time=False,
+            markup=True,
+            show_path=False,
+            console=rez_pip.utils.CONSOLE,
         )
         handler.setFormatter(logging.Formatter(fmt="%(message)s"))
 

src/rez_pip/download.py
@@ -5,6 +5,7 @@ import typing
 import asyncio
 import hashlib
 import logging
+import collections
 
 import aiohttp
 import rich.progress
@@ -58,28 +59,24 @@ async def _downloadPackages(
             # Then create the "total" progress bar. This ensures that total is at the bottom.
             mainTask = progress.add_task(f"[bold]Total (0/{numPackages})", total=0)
 
-            futureGroups: list[
-                list[
-                    typing.Coroutine[
-                        typing.Any,
-                        typing.Any,
-                        rez_pip.pip.DownloadedArtifact | None,
-                    ]
+            futures: list[
+                typing.Coroutine[
+                    typing.Any,
+                    typing.Any,
+                    rez_pip.pip.DownloadedArtifact | None,
                 ]
             ] = []
 
-            # loop = asyncio.get_event_loop()
-            for group in packageGroups:
-                futures: list[
-                    typing.Coroutine[
-                        typing.Any,
-                        typing.Any,
-                        rez_pip.pip.DownloadedArtifact | None,
-                    ]
-                ] = []
+            groupMapping: collections.defaultdict[int, list[str]] = (
+                collections.defaultdict(list)
+            )
+
+            for index, group in enumerate(packageGroups):
                 for package in group.packages:
                     wheelName: str = os.path.basename(package.download_info.url)
                     wheelPath = os.path.join(dest, wheelName)
+
+                    groupMapping[index].append(package.name)
 
                     if not package.isDownloadRequired():
 
@@ -105,24 +102,28 @@ async def _downloadPackages(
                                 wheelPath,
                             )
                         )
-                futureGroups.append(futures)
 
-            for _futures in futureGroups:
-                artifacts = tuple(await asyncio.gather(*_futures))
+            artifacts = tuple(await asyncio.gather(*futures))
 
-                if not all(artifacts):
-                    someFailed = True
+            if not all(artifacts):
+                raise RuntimeError("Some wheels failed to be downloaded")
 
-                artifacts = typing.cast(
-                    typing.Tuple[rez_pip.pip.DownloadedArtifact], artifacts
-                )
+            artifacts = typing.cast(
+                typing.Tuple[rez_pip.pip.DownloadedArtifact], artifacts
+            )
 
+            # Return artifacts in the same groups as they arrived in.
+            # TODO: The amount of looping and gymnastic is a big code smell.
+            for packageNames in groupMapping.values():
                 newPackageGroups.append(
-                    rez_pip.pip.PackageGroup[rez_pip.pip.DownloadedArtifact](artifacts)
+                    rez_pip.pip.PackageGroup[rez_pip.pip.DownloadedArtifact](
+                        tuple(
+                            artifact
+                            for artifact in artifacts
+                            if artifact.name in packageNames
+                        )
+                    )
                 )
-
-    if someFailed:
-        raise RuntimeError("Some wheels failed to be downloaded")
 
     return newPackageGroups
 
@@ -192,9 +193,9 @@ async def _download(
                 async for chunk, asd in response.content.iter_chunks():
                     if not chunk:
                         break
+                    fd.write(chunk)
                     progress.update(taskID, advance=len(chunk))
                     progress.update(mainTaskID, advance=len(chunk))
-                    fd.write(chunk)
 
             _LOG.info(
                 f"Downloaded {package.name}-{package.version} to {wheelPath!r} ({os.stat(wheelPath).st_size} bytes)"